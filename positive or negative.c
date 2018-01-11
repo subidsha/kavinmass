0954edc48bf24eefc0c5b2544526903b680d0037
kavinkumarkandasamy1998 <31652785+kavinkumarkandasamy1998@users.noreply.github.com>
2018-01-11 16:46:01 +0530

    Update and rename check number to positive or negative.c

---
+++ positive or negative.c
@@ -0,0 +1,11 @@
+#include <stdio.h>
+void main()
+{
+int number;
+printf("Enter a number: ");
+scanf("%d", &number);
+if (number >= 0)
+printf("%d is a positive number",number);
+else
+printf("%d is a negative number",number);
+}
