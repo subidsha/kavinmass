1e981bc72b4c8600991b4527dc83119300f9edf7
kavinkumarkandasamy1998 <31652785+kavinkumarkandasamy1998@users.noreply.github.com>
2018-01-18 16:27:58 +0530

    Create 2 num n,k array n int sum first k int .c

---
+++ 2 num n,k array n int sum first k int .c
@@ -0,0 +1,11 @@
+#include<stdio.h>
+main()
+{
+int m,a,k,s=0,a[n];
+scanf("%d%d",&a,&k);
+for(m=0;m<n;m++)
+scanf("%d",&a[m]);
+for(m=0;m<k;m++)
+s=s+a[m];
+printf("sum of 1st %d num is %d",k,s);
+}
