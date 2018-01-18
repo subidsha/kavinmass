d2ba85621a442b155af9fb29f38bc22e5f627d13
kavinkumarkandasamy1998 <31652785+kavinkumarkandasamy1998@users.noreply.github.com>
2018-01-18 16:42:53 +0530

    Update pali or not.c

--- pali or not.c
+++ pali or not.c
@@ -1,17 +1,17 @@
 #include<stdio.h>
 void main()
 {
-int n,n1,rem,rev=0;
+int n1,n2,rem,rev=0;
 printf("Enter any number");
-scanf("%d",&n);
-n1=n;
-while(n>0)
+scanf("%d",&n1);
+n2=n1;
+while(n1>0)
 {
-rem=n%10;
+rem=n1%10;
 rev=rev*10+rem;
-n=n/10;
+n1=n1/10;
 }
-if(n1==rev)
+if(n2==rev)
 printf("The given number is a palindrome");
 else
 printf("The given number is not a palindrome");
